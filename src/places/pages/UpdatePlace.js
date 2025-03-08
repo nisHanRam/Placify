a110aa454fe4818fc0aedac2cbba95ed801635d3
Nishant Choubisa <nishantchoubisa0@gmail.com>
2025-03-08 19:11:33 +0530

    Refactor. Custom Form Hook

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import useForm from "../../shared/hooks/form-hook";
@@ -41,20 +41,39 @@ const DUMMY_PLACES = [
 
 const UpdatePlace = () => {
   const placeId = useParams().placeId;
-  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
-  const [formState, inputHandler] = useForm(
+  const [formState, inputHandler, setFormData] = useForm(
     {
-      title: { value: identifiedPlace.title, isValid: true },
-      description: { value: identifiedPlace.description, isValid: true },
+      title: { value: "", isValid: false },
+      description: { value: "", isValid: false },
     },
-    true
+    false
   );
+
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+  
+  useEffect(() => {
+    setFormData(
+      {
+        title: { value: identifiedPlace.title, isValid: true },
+        description: { value: identifiedPlace.description, isValid: true },
+      },
+      true
+    );
+  }, [setFormData, identifiedPlace]);
 
   if (!identifiedPlace) {
     return (
       <div className="center">
         <h2>Could not find the place!</h2>
+      </div>
+    );
+  }
+
+  if (!formState.inputs.title.value) {
+    return (
+      <div className="center">
+        <h2>Loading...</h2>
       </div>
     );
   }
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -32,7 +32,7 @@ const Input = (props) => {
   const { value, isValid } = inputState;
 
   useEffect(() => {
-    props.onInput(props.id, inputState.value, inputState.isValid);
+    onInput(id, value, isValid);
   }, [id, onInput, value, isValid]);
 
   const changeHandler = (event) => {
